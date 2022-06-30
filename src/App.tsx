eeb53524d327995e3137bb8534345b71a3e706cd
Rafa Kuchta <rafal.kuchta1@gmail.com>
2022-06-30 12:42:45 +0200

    Added BrowserRouter.

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,31 @@ import React, {useState} from 'react';
 
 import { Header } from './components/layout/Header';
 import { Tasks } from './components/Tasks/Tasks';
-import {DoneTasks} from "./components/Tasks/Done/DoneTasks";
 import { SearchContext } from './context/search.context';
 import { LoadingContext } from './context/loading.context';
+import { Routes, Route } from 'react-router-dom';
+import {EditTask} from "./components/Tasks/Edit/EditTask";
 
 export const App = () => {
     const [search, setSearch] = useState('');
     const [loading, setLoading] = useState(false);
+
     return (
         <>
             <SearchContext.Provider value={{search, setSearch}}>
                 <LoadingContext.Provider value={{loading, setLoading}}>
-                    <Header />
-                    <Tasks />
-                    <DoneTasks />
+
+                    <Routes>
+
+                        <Route path="/" element={
+                            <>
+                            <Header />
+                            <Tasks />
+                            </>
+                        }/>
+                        <Route path="/edit/:id" element={<EditTask />}/>
+
+                    </Routes>
                 </LoadingContext.Provider>
             </SearchContext.Provider>
         </>
